6c5f9a9c79fd6ed9c14ce3db1862d744abd134f9
krozv <krozv.code@gmail.com>
2024-02-08 14:22:44 +0900

    Upload file

--- Practice/조금 더 어려운 연습 문제/당근 포장하기.py
+++ Practice/조금 더 어려운 연습 문제/당근 포장하기.py
@@ -1,4 +1,4 @@
-# 당근 포장하기 - 나중에 풀기
+# 당근 포장하기
 """
 당근 대/중/소
 조건1. 한 상자에 N//2개를 초과하는 당근X
@@ -8,44 +8,64 @@ N개의 당근
 포장 못하면 -1
 포장할 수 있으며 각 사
 """
-import sys
-sys.stdin = open('carrot.txt', 'r')
+def yammy_carrot(cnt, N):
+    while True:
+        boxes = []
+        box = []
+        min_carrot = N
+        max_carrot = 0
+        print(N // 3 + N % 3 - 1, round(N / 2))
+        for i in range(31):
+            if cnt[i] > 0:
+                # 조건1. 한 상자에 N//2개를 초과하는 당근X
+                print(i, len(box))
+                if len(boxes) == 3:
+                    box += [i] * cnt[i]
+                elif len(box) + cnt[i] < N//3 + N%3 -1:
+                    box += [i] * cnt[i]
+                elif N//3 + N%3 -1 <= len(box) + cnt[i] < round(N/2):
+                    box += [i] * cnt[i]
+                else:
+                    boxes += [box]
+                    if len(box) < min_carrot:
+                        min_carrot = len(box)
+                    if len(box) > max_carrot:
+                        max_carrot = len(box)
+                    box = [i] * cnt[i]
+        else:
+            if len(box) < min_carrot:
+                min_carrot = len(box)
+            if len(box) > max_carrot:
+                max_carrot = len(box)
+            boxes += [box]
+            return boxes, len(boxes), max_carrot-min_carrot
 
+
+import sys
+sys.stdin = open("sample_in.txt")
 T = int(input())
 for t in range(1, T+1):
-    print(t)
-    N = int(input())
-    carrots = list(map(int, input().split()))
-    carrot_cnt = [0] * 31
-    # 무게 별 carrot 몇 개?
-    for carrot in carrots:
-        carrot_cnt[carrot] += 1
-    # 박스 포장 가능 여부 확인
-    if max(carrot_cnt) > round(N/2):    # 박스 포장 불가
-        print(-1)
-    else:   # 박스 포장 가능
-        boxes = [0]*3   # 박스 미리 접어놓음
-        for i in range(31):      # 박스 포장 시작
+    N = int(input())    # 당근 개수, 3 <= N <= 1000
+    C = list(map(int, input().split()))     # 1 <= C <= 30
+    cnt = [0] * 31
+    # 당근 크기 별로 카운트를 센다
+    for i in range(N):
+        cnt[C[i]] += 1
+    boxes, box_cnt, carrot = yammy_carrot(cnt, N)
+    print(boxes)
+    # if box_cnt == 3:
+    #     re_boxes, box_cnt, re_carrot = yammy_carrot(cnt, (N//3 + N%3))
+    #     print(re_boxes)
+    #     if carrot > re_carrot:
+    #         carrot = re_carrot
+    #     print(f'#{t} {carrot}')
+    # else:
+    #     print(f'#{t} -1')
+                # 조건2. 각 상자에 든 당근의 개수 차이가 최소가 되도록 포장 -> 개수 차이 서류에 표시
 
-            if sum(boxes) == N:
-                break
-            if carrot_cnt[i] + boxes[0] <= N//2:                    # 포장 가능
-                if (N+1)//3 < carrot_cnt[i] + boxes[0] <= N//2:     # 걍 포장
-                    boxes[1] += carrot_cnt[i]
-                elif carrot_cnt[i] + boxes[0] <= (N+1)//3:          # 최적화 포장
-                    boxes[0] += carrot_cnt[i]
-            else:
-                print(i)
-                if carrot_cnt[i] + boxes[1] <= N//2:
-                    print((N + 1) // 3, N // 2)
-                    print(carrot_cnt[i] + boxes[1])
-                    if (N+1)//3 < carrot_cnt[i] + boxes[1] <= N//2:  # 걍 포장
-                        boxes[2] += carrot_cnt[i]
-                    elif carrot_cnt[i] + boxes[1] <= (N+1)//3:  # 최적화 포장
-                        boxes[1] += carrot_cnt[i]
-                    else:
-                        boxes[1] += carrot_cnt[i]
-                else:
-                    boxes[2] += carrot_cnt[i]
-            print(boxes)
 
+
+        # 넘을 경우 새로운 리스트에 추가
+        # N//2까지만 담고 이후로는 다음 리스트에 담음
+        # 만족한다면 재정렬
+            # N//3까지만 담음
