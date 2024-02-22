bd4ca6267bb64104f1e3162cdd3b3c3be572a525
krozv <krozv.code@gmail.com>
2024-02-22 11:15:03 +0900

    upload

--- APS_application/240222_Start/assignment.py
+++ APS_application/240222_Start/assignment.py
@@ -1,4 +1,7 @@
 # 진법 변환
+"""
+input된 값을 진법 변환하여 출력
+"""
 arr = [[0]*3 for _ in range(16)]
 print(0x11)
 for i in range(16):
@@ -8,3 +11,4 @@ for i in range(16):
 test = list(map(int, input()))
 print(test)
 for num in test:
+    pass
