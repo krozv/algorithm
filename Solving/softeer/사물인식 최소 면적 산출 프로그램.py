1e29f987ae45953a78102358439b39260cdb9e06
krozv <krozv.code@gmail.com>
2024-03-27 08:48:36 +0900

    upload

--- Solving/softeer/사물인식 최소 면적 산출 프로그램.py
+++ Solving/softeer/사물인식 최소 면적 산출 프로그램.py
@@ -7,11 +7,13 @@ for _ in range(N):
     x, y, k = map(int, input().split())
     arr[x+1000][y+1000].append(k)
     print(arr[x+1000][y+1000])
-visited = [0] * (K+1)
+
 # 완전탐색하는 부분 다시 짜세용
 for d in range(2000):
     for i in range(2001):
         for j in range(2001):
+            visited = [0] * (K + 1)
+            # 여기서 d만큼 반복
             if arr[i][j]:
                 for color in arr[i][j]:
                     visited[color] = 1
