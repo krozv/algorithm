3c5c6f1537535df206e503c21ffef55114710515
krozv <krozv.code@gmail.com>
2024-06-13 00:55:52 +0900

    test

---
+++ C++/Basic/test2/test2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  test2
+//
+//  Created by Park JuYeon on 6/13/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
