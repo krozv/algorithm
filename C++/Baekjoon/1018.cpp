ba6e7b56c5d8701b312b36e1859f2d4b0513fc9a
krozv <krozv.code@gmail.com>
2024-07-08 14:09:34 +0900

    solved 1018

--- C++/Baekjoon/1018.cpp
+++ C++/Baekjoon/1018.cpp
@@ -1,6 +1,8 @@
 // 1018. 체스판 다시 칠하기
 #include <iostream>
 #include <string>
+#include <algorithm>
+
 using namespace std;
 int main(){
     freopen("input.txt", "r", stdin);
@@ -9,7 +11,7 @@ int main(){
     int arr[50][50];
     for (int i=0; i<n; i++){
         string str;
-        getline(cin, str);
+        cin >> str;
         for (int j=0; j<m; j++){
             if (str[j] == 'W'){
                 arr[i][j] = 0;
@@ -19,9 +21,48 @@ int main(){
             }
         }
     }
-    for (int i=0; i<n; i++){
-        for (int j=0;)
+    int cnt=64;
+    for (int i=0; i<n-7; i++){
+        for (int j=0; j<m-7; j++){
+            // 시작을 0으로 가정
+            int temp = 1;
+            int countFromZero = 0;
+            for (int k=0; k<8; k++){
+                temp = 1- temp;
+                for (int l=0; l<8; l++){
+                    if (temp == arr[i+k][j+l]){
+                        countFromZero++;
+                    }
+                    temp = 1 - temp;
+                }
+            }
+            // 시작을 1로 가정
+            temp = 0;
+            int countFromOne = 0;
+            for (int k=0; k<8; k++){
+                temp = 1 - temp;
+                for (int l=0; l<8; l++){
+                    if (temp == arr[i+k][j+l]){
+                        countFromOne++;
+                    }
+                    temp = 1 - temp;
+                }
+            }
+            // cout << "countFromZero: " << countFromZero << endl;
+            // cout << "countFromOne: " << countFromOne << endl;
+
+            if (countFromOne > countFromZero){
+                cnt = min(cnt, countFromZero);
+                // cout << "zero" << endl;
+            }
+            else {
+                cnt = min(cnt, countFromOne);
+                // cout << "one" << endl;
+            }
+
+        }
     }
+    cout << cnt;
     // 입력을 W -> 0, B -> 1로 바꿔서 배열에 받을 것
     // 완전탐색으로 개수를 셀 예정
     // 1과 0을 시작으로 가정한 두 케이스에 대해서 cnt 개수 세서 최소값 도출하기
