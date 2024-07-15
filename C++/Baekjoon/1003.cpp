7d5b9955da32d33c921eac35e625d1f3db27aaa8
krozv <krozv.code@gmail.com>
2024-07-15 18:05:27 +0900

    upload

---
+++ C++/Baekjoon/1003.cpp
@@ -0,0 +1,14 @@
+// 1003.피보나치 함수
+#include <iostream>
+using namespace std;
+int main(){
+    // testcase T
+    int T;
+    cin >> T;
+    for (int i=0; i<T; i++){
+        // N이 주어진다
+        int N;
+        cin >> N;
+        
+    }
+}
